f92e33b671ba42c1dad947d5e7cc4155357f01f3
tayarsutayar <achmaddaniar@gmail.com>
2023-08-22 20:51:45 +0700

    Fix: restore session

--- index.ts
+++ index.ts
@@ -1,12 +1,28 @@
-import makeWASocket, { AnyMessageContent, delay, makeCacheableSignalKeyStore, makeInMemoryStore, proto, useMultiFileAuthState,  WASocket } from '@whiskeysockets/baileys'
+import { Boom } from '@hapi/boom'
+import NodeCache from 'node-cache'
+import readline from 'readline'
+import makeWASocket, { AnyMessageContent, delay, DisconnectReason, WASocket, fetchLatestBaileysVersion, makeCacheableSignalKeyStore, makeInMemoryStore, proto, useMultiFileAuthState, WAMessageContent, WAMessageKey } from '@whiskeysockets/baileys'
 import MAIN_LOGGER from './logger'
 import { assistant } from './assistant'
-import 'dotenv/config'
-
 const logger = MAIN_LOGGER.child({})
-logger.level = 'info'
+logger.level = 'trace'
 
-const store = makeInMemoryStore({ logger })
+const useStore = !process.argv.includes('--no-store')
+const doReplies = !process.argv.includes('--no-reply')
+const usePairingCode = process.argv.includes('--use-pairing-code')
+const useMobile = process.argv.includes('--mobile')
+
+// external map to store retry counts of messages when decryption/encryption fails
+// keep this out of the socket itself, so as to prevent a message decryption/encryption loop across socket restarts
+const msgRetryCounterCache = new NodeCache()
+
+// Read line interface
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+const question = (text: string) => new Promise<string>((resolve) => rl.question(text, resolve))
+
+// the store maintains the data of the WA connection in memory
+// can be written out to a file & read from it
+const store = useStore ? makeInMemoryStore({ logger }) : undefined
 store?.readFromFile('./baileys_store_multi.json')
 // save every 10s
 setInterval(() => {
@@ -24,39 +40,83 @@ export const sendMessageWTyping = async(msg: AnyMessageContent, jid: string, soc
 
 	await sock.sendMessage(jid, msg)
 }
-
+// start a connection
 const startSocket = async() => {
 	const { state, saveCreds } = await useMultiFileAuthState('baileys_auth_info')
+	// fetch latest version of WA Web
+	const { version, isLatest } = await fetchLatestBaileysVersion()
+	console.log(`using WA v${version.join('.')}, isLatest: ${isLatest}`)
 
 	const sock = makeWASocket({
-		printQRInTerminal: true,
+		version,
+		logger,
+		printQRInTerminal: !usePairingCode,
+		mobile: useMobile,
 		auth: {
 			creds: state.creds,
+			/** caching makes the store faster to send/recv messages */
 			keys: makeCacheableSignalKeyStore(state.keys, logger),
 		},
+		msgRetryCounterCache,
+		generateHighQualityLinkPreview: true,
+		// ignore all broadcast messages -- to receive the same
+		// comment the line below out
+		// shouldIgnoreJid: jid => isJidBroadcast(jid),
+		// implement to handle retries & poll updates
+		getMessage,
 	})
 
 	store?.bind(sock.ev)
 
+	// Pairing code for Web clients
+	if(usePairingCode && !sock.authState.creds.registered) {
+		if(useMobile) {
+			throw new Error('Cannot use pairing code with mobile api')
+		}
+
+		const phoneNumber = await question('Please enter your mobile phone number:\n')
+		const code = await sock.requestPairingCode(phoneNumber)
+		console.log(`Pairing code: ${code}`)
+	}
+
+	// the process function lets you process all events that just occurred
+	// efficiently in a batch
 	sock.ev.process(
-
+		// events is a map for event name => event data
 		async(events) => {
+			// something about the connection changed
+			// maybe it closed, or we received all offline message or connection opened
+			if(events['connection.update']) {
+				const update = events['connection.update']
+				const { connection, lastDisconnect } = update
+				if(connection === 'close') {
+					// reconnect if not logged out
+					if((lastDisconnect?.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut) {
+						startSocket()
+					} else {
+						console.log('Connection closed. You are logged out.')
+					}
+				}
 
+				console.log('connection update', update)
+			}
+
+			// credentials updated -- save them
 			if(events['creds.update']) {
 				await saveCreds()
 			}
 
+			// received a new message
 			if(events['messages.upsert']) {
 				const upsert = events['messages.upsert']
 				console.log('recv messages ', JSON.stringify(upsert, undefined, 2))
 
 				if(upsert.type === 'notify') {
 					for(const msg of upsert.messages) {
-						if(!msg.key.fromMe) {
+						if(!msg.key.fromMe && doReplies) {
 							console.log('replying to', msg.key.remoteJid)
+							await sock!.readMessages([msg.key])
 							const result = await assistant(msg)
-							console.log(result)
-							
 							if(result && result[0])await sendMessageWTyping(result[0] , msg.key.remoteJid!, sock)
 						}
 					}
@@ -66,6 +126,16 @@ const startSocket = async() => {
 	)
 
 	return sock
+
+	async function getMessage(key: WAMessageKey): Promise<WAMessageContent | undefined> {
+		if(store) {
+			const msg = await store.loadMessage(key.remoteJid!, key.id!)
+			return msg?.message || undefined
+		}
+
+		// only if store is present
+		return proto.Message.fromObject({})
+	}
 }
 
 startSocket()
